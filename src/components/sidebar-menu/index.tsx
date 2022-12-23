932cf6186e75c6823813d29995d8ebe48375d416
EdsonPaulo <edsonpaulo24@gmail.com>
2022-12-23 11:28:08 +0100

    fix: sidebar

--- src/components/sidebar-menu/index.tsx
+++ src/components/sidebar-menu/index.tsx
@@ -1,74 +1,90 @@
 import { Box, Flex, Icon, Image, Link, Text } from '@chakra-ui/react';
 import { AdminMenu } from './menu-items';
+import { AiOutlinePoweroff } from 'react-icons/ai';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { ROUTES } from '../../constants/routes';
 
 export const SidebarMenu = () => {
   const { pathname } = useLocation();
 
   return (
-    <Box width={280} height="100vh" textColor="white" bgColor="brand.primary">
-      <Flex mb={12} justifyContent="center" p={10}>
-        <Image src="/farm-gest-logo.svg" alt="farm gest logo" />
-      </Flex>
+    <Flex
+      width={280}
+      height="100vh"
+      textColor="white"
+      bgColor="brand.primary"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Box>
+        <Flex mb={10} mt={6} p={10}>
+          <Link as={RouterLink} to={ROUTES.Dashboard}>
+            <Image src="/farm-gest-logo.svg" alt="farm gest logo" />
+          </Link>
+        </Flex>
 
-      {AdminMenu.map((item) => (
-        <Box key={item.title} mb={12} pl={10}>
-          <Text
-            mb={3}
-            fontSize={14}
-            textTransform="uppercase"
-            fontFamily="Roboto"
-            fontWeight="900"
-            letterSpacing={1.2}
-          >
-            {item.title}
-          </Text>
-
-          {item.children.map((menuItem) => (
-            <Link
-              key={menuItem.title}
-              as={RouterLink}
-              to={menuItem.path}
-              textDecoration="none"
+        {AdminMenu.map((item) => (
+          <Box key={item.title} mb={12} pl={10}>
+            <Text
+              mb={3}
+              fontSize={14}
+              textTransform="uppercase"
+              fontFamily="Roboto"
+              fontWeight="900"
+              letterSpacing={1.2}
             >
-              <Flex
-                mb={2}
-                py={2}
-                px={4}
-                alignItems="center"
-                cursor={'pointer'}
-                borderBottomLeftRadius={6}
-                borderTopLeftRadius={6}
-                bg={
-                  menuItem.path === pathname
-                    ? 'brand.primaryDark'
-                    : 'brand.primary'
-                }
-                _hover={{ background: 'brand.primaryDark' }}
-              >
-                <Icon
-                  mr={3}
-                  as={menuItem.icon}
-                  boxSize={5}
-                  color="white"
-                  textColor={'white'}
-                />
+              {item.title}
+            </Text>
 
-                <Text
-                  fontWeight="600"
-                  fontSize={16}
-                  letterSpacing={1.1}
-                  textTransform={'capitalize'}
+            {item.children.map((menuItem) => (
+              <Link
+                key={menuItem.title}
+                as={RouterLink}
+                to={menuItem.path}
+                textDecoration="none"
+              >
+                <Flex
+                  mb={2}
+                  py={2}
+                  px={4}
+                  alignItems="center"
+                  cursor={'pointer'}
+                  borderBottomLeftRadius={6}
+                  borderTopLeftRadius={6}
+                  bg={
+                    menuItem.path === pathname
+                      ? 'brand.primaryDark'
+                      : 'brand.primary'
+                  }
+                  _hover={{ background: 'brand.primaryDark' }}
                 >
-                  {menuItem.title}
-                </Text>
-              </Flex>
-            </Link>
-          ))}
-        </Box>
-      ))}
-      <Box />
-      <Box />
-    </Box>
+                  <Icon mr={3} as={menuItem.icon} boxSize={5} color="white" />
+                  <Text
+                    fontWeight="600"
+                    fontSize={16}
+                    letterSpacing={1.1}
+                    textTransform={'capitalize'}
+                  >
+                    {menuItem.title}
+                  </Text>
+                </Flex>
+              </Link>
+            ))}
+          </Box>
+        ))}
+      </Box>
+
+      <Flex
+        pb={20}
+        ml={10}
+        cursor="pointer"
+        _hover={{ textDecor: 'underline' }}
+      >
+        <Icon mr={3} boxSize={5} color="white" as={AiOutlinePoweroff} />
+        <Text fontWeight="600" fontSize={16} letterSpacing={1.1}>
+          Terminar sessão
+        </Text>
+      </Flex>
+    </Flex>
   );
 };
