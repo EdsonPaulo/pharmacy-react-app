1c62635daa6c5f01ddaedc00344180356d482598
EdsonPaulo <edsonpaulo24@gmail.com>
2023-01-26 22:05:21 +0100

    fix: allow signup

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -9,8 +9,6 @@ import {
   InputRightElement,
   Link,
   Text,
-  Alert,
-  AlertIcon,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
@@ -22,109 +20,106 @@ export const SignUpPage = () => {
   const { handleSignUp, isLoading, formData, handleChangeFormData } = useAuth();
 
   return (
-    <Flex minH="100vh" bg="brand.primary">
-      <Box flex={1} display={{ xs: 'none', md: 'block' }}>
-        <Box
-          minH="100%"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Image src="/farm-gest-logo.svg" />
+    <form>
+      <Flex minH="100vh" bg="brand.primary">
+        <Box flex={1} display={{ xs: 'none', md: 'block' }}>
+          <Box
+            minH="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Image src="/farm-gest-logo.svg" />
+          </Box>
         </Box>
-      </Box>
 
-      <Flex bg="white" flex={0.5} p={20} alignItems="center">
-        <Box w="100%">
-          <Alert status="warning" mb={4}>
-            <AlertIcon />A criação de conta está bloqueada temporariamente
-            porque o sistema está em desenvolvimento
-          </Alert>
+        <Flex bg="white" flex={0.5} p={20} alignItems="center">
+          <Box w="100%">
+            <Text fontSize="2xl" as="b">
+              Criar Conta
+            </Text>
 
-          <Text fontSize="2xl" as="b">
-            Criar Conta
-          </Text>
+            <Box h="4px" w="35px" bg="brand.primary" mt={1} mb={4} />
 
-          <Box h="4px" w="35px" bg="brand.primary" mt={1} mb={4} />
-
-          <Text mb={1}>Nome e Sobrenome</Text>
-          <Input
-            size="md"
-            required
-            value={formData.name}
-            placeholder="Utilizador Exemplo"
-            inputMode="text"
-            type="text"
-            autoComplete="none"
-            onChange={({ target: { value } }) =>
-              handleChangeFormData({ name: value })
-            }
-          />
-
-          <Text mt={3} mb={1}>
-            Email
-          </Text>
-          <Input
-            size="md"
-            required
-            value={formData.email}
-            placeholder="utilizador@email.com"
-            inputMode="email"
-            role="presentation"
-            autoComplete="off"
-            onChange={({ target: { value } }) =>
-              handleChangeFormData({ email: value })
-            }
-          />
-
-          <Text mt={3} mb={1}>
-            Palavra-passe
-          </Text>
-          <InputGroup size="md">
+            <Text mb={1}>Nome e Sobrenome</Text>
             <Input
               size="md"
               required
-              value={formData.password}
-              type={showPassword ? 'text' : 'password'}
-              placeholder="********"
-              autoComplete="new-password"
+              value={formData.name}
+              placeholder="Utilizador Exemplo"
+              inputMode="text"
+              type="text"
+              autoComplete="none"
               onChange={({ target: { value } }) =>
-                handleChangeFormData({ password: value })
+                handleChangeFormData({ name: value })
               }
             />
-            <InputRightElement>
-              <IconButton
-                icon={showPassword ? <FiEyeOff /> : <FiEye />}
-                onClick={() => setShowPassword((prev) => !prev)}
-                aria-label={'mostrar palavra-passe'}
+
+            <Text mt={3} mb={1}>
+              Email
+            </Text>
+            <Input
+              size="md"
+              required
+              type="email"
+              value={formData.email}
+              placeholder="utilizador@email.com"
+              inputMode="email"
+              role="presentation"
+              autoComplete="off"
+              onChange={({ target: { value } }) =>
+                handleChangeFormData({ email: value })
+              }
+            />
+
+            <Text mt={3} mb={1}>
+              Palavra-passe
+            </Text>
+            <InputGroup size="md">
+              <Input
+                size="md"
+                required
+                value={formData.password}
+                type={showPassword ? 'text' : 'password'}
+                placeholder="********"
+                autoComplete="new-password"
+                onChange={({ target: { value } }) =>
+                  handleChangeFormData({ password: value })
+                }
               />
-            </InputRightElement>
-          </InputGroup>
+              <InputRightElement>
+                <IconButton
+                  icon={showPassword ? <FiEyeOff /> : <FiEye />}
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  aria-label={'mostrar palavra-passe'}
+                />
+              </InputRightElement>
+            </InputGroup>
 
-          <Button
-            mt={6}
-            w="100%"
-            color="white"
-            variant="solid"
-            colorScheme="teal"
-            isLoading={isLoading}
-            disabled={
-              //! formData?.name || !formData?.email || !formData?.password
-              true
-            }
-            onClick={() => handleSignUp()}
-          >
-            Criar conta
-          </Button>
+            <Button
+              mt={6}
+              w="100%"
+              color="white"
+              variant="solid"
+              colorScheme="teal"
+              isLoading={isLoading}
+              disabled={
+                !formData?.name || !formData?.email || !formData?.password
+              }
+              onClick={() => handleSignUp()}
+            >
+              Criar conta
+            </Button>
 
-          <Text mt={10} textAlign="center">
-            Já tem uma conta?
-            <Link ml={2} as={RouterLink} to="/entrar" color="brand.primary">
-              Entrar
-            </Link>
-          </Text>
-        </Box>
+            <Text mt={10} textAlign="center">
+              Já tem uma conta?
+              <Link ml={2} as={RouterLink} to="/entrar" color="brand.primary">
+                Entrar
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </form>
   );
 };
